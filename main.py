0d27946580fa5b1cc7f2732d8deee9d50a7c439d
Aline Areda <aredaaline@gmail.com>
2024-05-15 00:46:08 -0300

    incluindo path parameter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing  import List, Optional
 from fastapi import FastAPI
 from fastapi import HTTPException
-from fastapi import status
+from fastapi import status, Path
 from fastapi.responses import JSONResponse, Response
 
 from models import Curso
@@ -26,7 +26,7 @@ async def get_cursos():
   return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3)):
   try:
     curso = cursos[curso_id]
     return curso
