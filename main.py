25b74c15352d79c7ac8df2ac85695d84a36649c9
Aline Areda <aredaaline@gmail.com>
2024-05-15 02:04:56 -0300

    alterando doc da API

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing  import List, Optional
+from typing  import Dict, List, Optional
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status, Path
@@ -6,7 +6,11 @@ from fastapi.responses import JSONResponse, Response
 
 from models import Curso
 
-app = FastAPI()
+app = FastAPI(
+  title= 'API de Cursos',
+  version= '0.0.1',
+  description= 'API para estudo do FastAPI'
+  )
 
 cursos = {
   1: {
@@ -21,11 +25,11 @@ cursos = {
   }   
 }
 
-@app.get('/cursos')
+@app.get('/cursos', description= 'Retorna todos os cursos ou uma lista vazia', summary='Buscar todos os cursos', response_description='Cursos encontrados com sucesso')
 async def get_cursos():
   return cursos
 
-@app.get('/cursos/{curso_id}')
+@app.get('/cursos/{curso_id}', description= 'Retorna um curso pelo id', summary='Buscar curso por ID')
 async def get_curso(curso_id: int = Path(title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3)):
   try:
     curso = cursos[curso_id]
@@ -33,14 +37,14 @@ async def get_curso(curso_id: int = Path(title='ID do curso', description='Deve 
   except KeyError:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado!')
 
-@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+@app.post('/cursos', status_code=status.HTTP_201_CREATED, description= 'Criar um novo curso', summary='Criar um novo curso', response_model=Curso)
 async def post_curso(curso: Curso):
   next_id: int = len(cursos) + 1  
   cursos[next_id] = curso
   del curso.id
   return curso
 
-@app.put('/cursos/{curso_id}')
+@app.put('/cursos/{curso_id}', description= 'Alterar informações do curso', summary='Alterar informações de curso')
 async def put_curso(curso_id: int, curso: Curso):
   if curso_id in cursos:
     cursos[curso_id] = curso
@@ -51,7 +55,7 @@ async def put_curso(curso_id: int, curso: Curso):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                         detail= f'Não existe um curso com o ID {curso_id}')
   
-@app.delete('/cursos/{curso_id}')
+@app.delete('/cursos/{curso_id}', description= 'Excluir um curso', summary='Excluir curso por ID')
 async def delete_curso(curso_id: int):
   if curso_id in cursos:
     del cursos[curso_id]
@@ -60,7 +64,7 @@ async def delete_curso(curso_id: int):
   else:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail= f'Não existe um curso com ID {curso_id}')
-  
+
   
 if __name__ == '__main__':
   import uvicorn
